8c851418ceaced17fd4df03518b0de2b6cda45ce
mssjeong <mssjong@yonsei.ac.kr>
2025-10-04 07:37:52 +0900

    Update gumbel.jl

--- examples/gumbel.jl
+++ examples/gumbel.jl
@@ -1,13 +1,12 @@
 # Gumbel distribution
 using Distributed
-using Pkg
 using CSV
 using DataFrames
 
 addprocs(Threads.nthreads(); exeflags=`--threads=1`)
 println("Number of workers: ",nworkers())
 
-@everywhere using ExactCI
+@everywhere using ExactConfidenceIntervals
 @everywhere using Distributions
 
 @everywhere function gen_gumbel(th,n)
@@ -25,3 +24,4 @@ df = CSV.read("gumbel.csv", DataFrame)
 y = df[:,"y"]
 
 th, ci = exactci(y, generator=gen_gumbel, likelihood=lik_gumbel, positive=[2])
+
